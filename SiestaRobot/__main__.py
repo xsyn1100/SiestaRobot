616634ccd5efd2bb149ecead144e2c3ec6912443
xsyn1100 <98083245+xsyn1100@users.noreply.github.com>
2022-03-07 04:10:49 +0700

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -417,7 +417,7 @@ def siesta_about_callback(update, context):
                             InlineKeyboardButton(text=gs(chat.id, "inline_button"), switch_inline_query_current_chat=""),
                         ],
                         [
-                            InlineKeyboardButton(text=gs(chat.id, "add_bot_to_group_button"), url="t.me/Siestaxbot?startgroup=new"),
+                            InlineKeyboardButton(text=gs(chat.id, "add_bot_to_group_button"), url="t.me/synaxrobot?startgroup=new"),
                         ]
                     ]
                 ),
