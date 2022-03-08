1759fdc3b1782a1d8e48edcd4f5f92d411ca7333
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-03-09 00:09:52 +0700

    Update welcome.py

--- SiestaRobot/modules/welcome.py
+++ SiestaRobot/modules/welcome.py
@@ -257,8 +257,6 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
                         \n•Creator ID:   `\\{creator.id}` \
                         \n•Creator Username:   \@{creator.username} \
                         """,
-                     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="ᴠɪᴇᴡs​​", url=f"https://t.me/{creator.username}")]]),
-                     disable_web_page_preview=True,
                      parse_mode=ParseMode.MARKDOWN_V2,
                     )
                 else:
